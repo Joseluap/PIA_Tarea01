2edfdd90049526dd3bc6b38ff9dde043773baaf9
Joseluap <119595672+Joseluap@users.noreply.github.com>
2022-12-01 14:54:29 +0100

    Update Hola_mundo.py

--- Hola_mundo.py
+++ Hola_mundo.py
@@ -1,6 +1,4 @@
 
-# Crear archivo Hola_Mundo en Python Tarea PIA01
+# Crear archivo Hola_mundo en Python Tarea PIA01
 
-print('------------------------------')
 print("Hola Mundo")
-print('------------------------------')
