74ea02e1e85bf4ace4b2c3ef3ef9d92edb61f74b
Joseluap <119595672+Joseluap@users.noreply.github.com>
2022-12-01 14:35:52 +0100

    Create Hola_mundo.py

---
+++ Hola_mundo.py
@@ -0,0 +1,14 @@
+
+# Crear archivo txt Python
+
+def createFile(content, path = 'files/Hola_mundo.txt'):
+	with open(path, 'w') as file:
+		file.write(content)
+
+print('------------------------------')
+print("Creación de archivos")
+print('------------------------------')
+
+fileName = input('Nombre: ')
+fileContent = input('Escriba el contenido: ')
+createFile(fileContent, f'files/{fileName}.txt')
